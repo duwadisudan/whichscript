fd8f2e252b086215fa12aa6bef121b1c4234a60a
Sudan Duwadi <75964384+duwadisudan@users.noreply.github.com>
2025-08-28 18:58:41 -0400

    Create sudan_plot_try.py

---
+++ sudan_plot_try.py
@@ -0,0 +1,10 @@
+#%%
+
+import whichscript
+whichscript.enable_auto_logging()
+
+#%%
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.savefig("U:\\eng_research_hrc_binauralhearinglab\\Sudan\\Labs\\Sen Lab\\whichscript\\plot_test_result\\my_plot.png")
